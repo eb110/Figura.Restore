44fc741d19205678ea7baeef935e1bd20648e132
Wladyslaw Figura <wfigura@op.pl>
2026-02-14 14:52:44 +0000

    create order

--- Figura.Restore.API/Controllers/OrdersController.cs
+++ Figura.Restore.API/Controllers/OrdersController.cs
@@ -1,4 +1,6 @@
 ﻿using Figura.Restore.API.Data;
+using Figura.Restore.API.DTOs;
+using Figura.Restore.API.Entities;
 using Figura.Restore.API.Entities.OrderAggregate;
 using Figura.Restore.API.Extensions;
 using Microsoft.AspNetCore.Authorization;
@@ -33,6 +35,55 @@ namespace Figura.Restore.API.Controllers
             if (order == null) return NotFound();
 
             return Ok(order);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Order>> CreateOrder(CreateOrderDto orderDto)
+        {
+            //the cookie 'basketId' travels as the reqest / response element
+            //extension method of the 'Basket' 
+            var basket = await context.Baskets.GetBasketWithItems(Request.Cookies["basketId"]);
+
+            if(basket == null || basket.Items.Count == 0) return NotFound("Basket is empty or not found");
+
+            var items = CreateOrderItems(basket.Items);
+
+            var subtotal = items.Sum(x => x.Price * x.Quantity);
+            var deliveryFee = CalculateDeliveryFee(subtotal);
+
+            var order = new Order
+            {
+                OrderItems = items,
+                BuyerEmail = User.GetUsername(),
+                ShippingAddress = orderDto.ShippingAddress,
+                DeliveryFee = deliveryFee,
+                Subtotal = subtotal,
+                PaymentSummary = orderDto.PaymentSummary,
+                PaymentIntentId = basket.PaymentIntentId,
+            };
+
+            context.Orders.Add(order);
+            //the order is finished - remove the basket!!!
+            context.Baskets.Remove(basket);
+            //the order is finished - remove the cookie!!!
+            Response.Cookies.Delete("basketId");
+
+            var result = await context.SaveChangesAsync() > 0;
+            if (!result) return BadRequest("problem creating order");
+
+            //this will create a location header
+            //where the client can find recently created order  
+            return CreatedAtAction(nameof(GetOrderDetails), new { id = order.Id }, order);
+        }
+
+        private long CalculateDeliveryFee(long subtotal)
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<OrderItem> CreateOrderItems(List<BasketItem> items)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Figura.Restore.API/DTOs/CreateOrderDto.cs
@@ -0,0 +1,14 @@
+﻿using Figura.Restore.API.Entities.OrderAggregate;
+
+namespace Figura.Restore.API.DTOs
+{
+    //staff needed from the browser
+    //rest of order parameters will be obtained either from the Basket or 
+    //will be calculated / created
+    public class CreateOrderDto
+    {
+        //we have to obtain these two parameters from the browser
+        public required ShippingAddress ShippingAddress { get; set; }
+        public required PaymentSummary PaymentSummary { get; set; }
+    }
+}
