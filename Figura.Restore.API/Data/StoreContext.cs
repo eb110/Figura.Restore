6c8fdc997d1cd2c564230a6bd23441c155804afa
Wladyslaw Figura <wfigura@op.pl>
2026-02-14 13:51:42 +0000

    Order entity

--- Figura.Restore.API/Data/StoreContext.cs
+++ Figura.Restore.API/Data/StoreContext.cs
@@ -1,4 +1,5 @@
 ﻿using Figura.Restore.API.Entities;
+using Figura.Restore.API.Entities.OrderAggregate;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ namespace Figura.Restore.API.Data
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Order> Orders { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
---
+++ Figura.Restore.API/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,26 @@
+﻿namespace Figura.Restore.API.Entities.OrderAggregate
+{
+    //all of the 'owned' classes will be inside order table
+    public class Order
+    {
+        public int Id { get; set; }
+        public required string BuyerEmail { get; set; }
+        //owned
+        public required ShippingAddress ShippingAddress { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        public List<OrderItem> OrderItems { get; set; } = [];
+        public long Subtotal { get; set; }
+        public long DeliveryFee { get; set; }
+        public long Discount { get; set; }
+        //in theory it always should exist but as its a payment - we will be defensive here
+        public string? PaymentIntentId { get; set; }
+        public OrderStatus OrderStatus { get; set; } = OrderStatus.Pending;
+        //owned
+        public required PaymentSummary PaymentSummary { get; set; }
+
+        public long GetTotal()
+        {
+            return Subtotal + DeliveryFee - Discount;
+        }
+    }
+}
