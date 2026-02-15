9e16200d644383c763fdc3cc7944e6fd905f7942
Wladyslaw Figura <wfigura@op.pl>
2026-02-15 17:52:06 +0000

    code clean up

--- Figura.Restore.API/Entities/OrderAggregate/PaymentSummary.cs
+++ Figura.Restore.API/Entities/OrderAggregate/PaymentSummary.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 namespace Figura.Restore.API.Entities.OrderAggregate
 {
     [Owned] //no id -> no db table -> part of the order
+    //Json properties - exp_month and exp_year comes from stripe transaction response parameter
+    //we could either update it on the client (naming) or just adopt it by JsonPropertyName
     public class PaymentSummary
     {
         //cc details
         public int Last4 { get; set; }
         public required string Brand { get; set; }
+        [JsonPropertyName("exp_month")]
         public int ExpMonth { get; set; }
+        [JsonPropertyName("exp_year")]
         public int ExpYear { get; set; }
     }
 }
--- Figura.Restore.API/Extensions/OrderExtenstions.cs
+++ Figura.Restore.API/Extensions/OrderExtenstions.cs
@@ -25,7 +25,7 @@ namespace Figura.Restore.API.Extensions
                     Name = item.ItemOrdered.Name,
                     PictureUrl = item.ItemOrdered.PictureUrl,
                     Price = item.Price,
-                    Quentity = item.Quantity
+                    Quantity = item.Quantity
                 }).ToList()
             }).AsNoTracking();
         }
@@ -49,7 +49,7 @@ namespace Figura.Restore.API.Extensions
                     Name = item.ItemOrdered.Name,
                     PictureUrl = item.ItemOrdered.PictureUrl,
                     Price = item.Price,
-                    Quentity = item.Quantity
+                    Quantity = item.Quantity
                 }).ToList()
             };
         }
